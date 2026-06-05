8820c1ac3210c1bbf887cda50659e9ba6753afec
PorebskiCode18 <damianfc37@gmail.com>
2026-06-05 15:06:17 -0500

    SkipSpaces

--- HiEqualshi.cpp
+++ HiEqualshi.cpp
@@ -2,8 +2,16 @@
 #include <cctype>
 #include <cassert>
 
-int strcmp_case_insensitive(const char* str1, const char* str2) {
+int strcmp_case_insensitive(const char* str1, const char* str2,bool skipSpaces = false) {
     while (*str1 && *str2) {
+        if (skipSpaces){
+            while (*str1 == ' ') {
+                ++str1;
+            }
+            while (*str2 == ' ') {
+                ++str2;
+            }    
+        }
         char c1 = std::tolower(*str1);
         char c2 = std::tolower(*str2);
         
@@ -28,6 +36,16 @@ int main() {
     assert(strcmp_case_insensitive("", "") == 0);
 
     assert(strcmp_case_insensitive("cat", "cats") < 0);
+
+    assert(strcmp_case_insensitive("my cool string","mycoolstring",true) == 0);
+
+    assert(strcmp_case_insensitive("My Cool String","MyCoolString",true) == 0);
+
+    assert(strcmp_case_insensitive("My Cool String","mycoolstring",true) == 0);
+
+    assert(strcmp_case_insensitive(" My Cool String","mycoolstring",true) == 0);
+
+    assert(strcmp_case_insensitive("Another string","mycoolstring",true) != 0);
 
     std::cout << "All tests passed!" << std::endl;
 
